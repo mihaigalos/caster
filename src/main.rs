d4ccfd1577856aaf10e1159f6a495ea43905e823
Mihai Galos <mihaigalos@gmail.com>
2022-02-11 22:23:04 +0100

    Separate execute()

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,31 @@
 use autoclap::autoclap;
 use clap::App;
 use clap::Arg;
+use std::vec::Drain;
 
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 
 use std::process::Command;
+
+async fn execute<'a>(command: &str, args: Drain<'_, &'a str>) -> String {
+    let output = Command::new(command)
+        .args(args)
+        .output()
+        .expect("failed to execute command");
+    let mut result = String::from_utf8(output.stdout).unwrap();
+
+    if output.status.code().unwrap() != 0 {
+        result = result
+            + "Exit status: "
+            + &output.status.code().unwrap().to_string()
+            + "\n\n"
+            + "stderr:\n"
+            + &String::from_utf8(output.stderr).unwrap()
+            + "\n";
+    }
+    result
+}
 
 async fn command(explicit_command: &str, input: Request<Body>) -> String {
     let whole_body = hyper::body::to_bytes(input.into_body()).await.unwrap();
@@ -24,22 +44,7 @@ async fn command(explicit_command: &str, input: Request<Body>) -> String {
         args = command_with_args.drain(0..);
     }
 
-    let output = Command::new(command)
-        .args(args)
-        .output()
-        .expect("failed to execute command");
-    let mut result = String::from_utf8(output.stdout).unwrap();
-
-    if output.status.code().unwrap() != 0 {
-        result = result
-            + "Exit status: "
-            + &output.status.code().unwrap().to_string()
-            + "\n\n"
-            + "stderr:\n"
-            + &String::from_utf8(output.stderr).unwrap()
-            + "\n";
-    }
-    return result;
+    return execute(command, args).await;
 }
 
 async fn service(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
