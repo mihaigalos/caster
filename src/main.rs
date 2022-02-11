e25739cf24585e45c0ed8f00ef29d8007bf91f8c
Mihai Galos <mihaigalos@gmail.com>
2022-02-11 19:48:49 +0100

    Add curl, ping endpoints

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,50 @@ async fn command(input: Request<Body>) -> String {
     return result;
 }
 
+async fn ping(input: Request<Body>) -> String {
+    let whole_body = hyper::body::to_bytes(input.into_body()).await.unwrap();
+    let cmd = String::from_utf8(whole_body.iter().cloned().collect::<Vec<u8>>()).unwrap();
+    let command_with_args: Vec<&str> = cmd.split_whitespace().collect();
+    let output = Command::new("ping")
+        .args(&command_with_args)
+        .output()
+        .expect("failed to execute command");
+    let mut result = String::from_utf8(output.stdout).unwrap();
+
+    if output.status.code().unwrap() != 0 {
+        result = result
+            + "Exit status: "
+            + &output.status.code().unwrap().to_string()
+            + "\n\n"
+            + "stderr:\n"
+            + &String::from_utf8(output.stderr).unwrap()
+            + "\n";
+    }
+    return result;
+}
+
+async fn curl(input: Request<Body>) -> String {
+    let whole_body = hyper::body::to_bytes(input.into_body()).await.unwrap();
+    let cmd = String::from_utf8(whole_body.iter().cloned().collect::<Vec<u8>>()).unwrap();
+    let command_with_args: Vec<&str> = cmd.split_whitespace().collect();
+    let output = Command::new("curl")
+        .args(&command_with_args)
+        .output()
+        .expect("failed to execute command");
+    let mut result = String::from_utf8(output.stdout).unwrap();
+
+    if output.status.code().unwrap() != 0 {
+        result = result
+            + "Exit status: "
+            + &output.status.code().unwrap().to_string()
+            + "\n\n"
+            + "stderr:\n"
+            + &String::from_utf8(output.stderr).unwrap()
+            + "\n";
+    }
+    return result;
+}
+
 async fn service(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => Ok(Response::new(Body::from(
@@ -34,6 +78,8 @@ async fn service(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
         ))),
 
         (&Method::POST, "/") => Ok(Response::new(Body::from(command(req).await))),
+        (&Method::POST, "/ping") => Ok(Response::new(Body::from(ping(req).await))),
+        (&Method::POST, "/curl") => Ok(Response::new(Body::from(curl(req).await))),
 
         _ => {
             let mut not_found = Response::default();
