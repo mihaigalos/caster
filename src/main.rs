f64236463fffb4657fa8daaec63d42d74f74e052
Mihai Galos <mihaigalos@gmail.com>
2022-02-11 21:43:05 +0100

    Add --secure

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![deny(warnings)]
 
+use autoclap::autoclap;
+use clap::App;
+use clap::Arg;
+
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 
@@ -12,16 +16,13 @@ async fn command(explicit_command: &str, input: Request<Body>) -> String {
 
     let command;
     let args;
-    if explicit_command != "" {
-        command = explicit_command;
-        args = command_with_args.drain(0..);
-    } else {
+    if explicit_command == "" {
         command = command_with_args[0];
         args = command_with_args.drain(1..);
+    } else {
+        command = explicit_command;
+        args = command_with_args.drain(0..);
     }
-
-    println!("{}", command);
-    println!("{:?}", args);
 
     let output = Command::new(command)
         .args(args)
@@ -59,13 +60,45 @@ async fn service(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     }
 }
 
+async fn service_secure(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Ok(Response::new(Body::from(
+            "Example usage:\n  curl localhost:8080 -XPOST -d 'ls'\n",
+        ))),
+
+        (&Method::POST, "/ping") => Ok(Response::new(Body::from(command("ping", req).await))),
+        (&Method::POST, "/curl") => Ok(Response::new(Body::from(command("curl", req).await))),
+
+        _ => Ok(Response::new(Body::from(
+            "Server started with --secure. Only explicit endpoints like /ping and /curl are available.\n",
+        ))),
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let app: clap::App = autoclap!();
+    let args = app
+        .arg(
+            Arg::new("secure")
+                .long("secure")
+                .short('s')
+                .help("Only run explicit commands from endpoints such as /ping and /curl to avoid sensitive data leakage."),
+        )
+        .try_get_matches()
+        .unwrap_or_else(|e| e.exit());
     let addr = ([0, 0, 0, 0], 8080).into();
-    let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(service)) });
-    let server = Server::bind(&addr).serve(service);
 
+    if args.is_present("secure") {
+        let service =
+            make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(service_secure)) });
+        let server = Server::bind(&addr).serve(service);
+        server.await?;
+    } else {
+        let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(service)) });
+        let server = Server::bind(&addr).serve(service);
+        server.await?;
+    }
     println!("Listening on http://{}", addr);
-    server.await?;
     Ok(())
 }
