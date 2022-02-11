6f2fc6541f80544d8868ef107f803d35ce7fb8c0
Mihai Galos <mihaigalos@gmail.com>
2022-02-11 20:25:52 +0100

    Compact curl and ping commands in one function

--- src/main.rs
+++ src/main.rs
@@ -5,56 +5,26 @@ use hyper::{Body, Method, Request, Response, Server, StatusCode};
 
 use std::process::Command;
 
-async fn command(input: Request<Body>) -> String {
+async fn command(explicit_command: &str, input: Request<Body>) -> String {
     let whole_body = hyper::body::to_bytes(input.into_body()).await.unwrap();
     let cmd = String::from_utf8(whole_body.iter().cloned().collect::<Vec<u8>>()).unwrap();
-    let command_with_args: Vec<&str> = cmd.split_whitespace().collect();
-    let output = Command::new(command_with_args[0])
-        .args(&command_with_args[1..])
-        .output()
-        .expect("failed to execute command");
-    let mut result = String::from_utf8(output.stdout).unwrap();
+    let mut command_with_args: Vec<&str> = cmd.split_whitespace().collect();
 
-    if output.status.code().unwrap() != 0 {
-        result = result
-            + "Exit status: "
-            + &output.status.code().unwrap().to_string()
-            + "\n\n"
-            + "stderr:\n"
-            + &String::from_utf8(output.stderr).unwrap()
-            + "\n";
+    let command;
+    let args;
+    if explicit_command != "" {
+        command = explicit_command;
+        args = command_with_args.drain(0..);
+    } else {
+        command = command_with_args[0];
+        args = command_with_args.drain(1..);
     }
-    return result;
-}
 
-async fn ping(input: Request<Body>) -> String {
-    let whole_body = hyper::body::to_bytes(input.into_body()).await.unwrap();
-    let cmd = String::from_utf8(whole_body.iter().cloned().collect::<Vec<u8>>()).unwrap();
-    let command_with_args: Vec<&str> = cmd.split_whitespace().collect();
-    let output = Command::new("ping")
-        .args(&command_with_args)
-        .output()
-        .expect("failed to execute command");
-    let mut result = String::from_utf8(output.stdout).unwrap();
+    println!("{}", command);
+    println!("{:?}", args);
 
-    if output.status.code().unwrap() != 0 {
-        result = result
-            + "Exit status: "
-            + &output.status.code().unwrap().to_string()
-            + "\n\n"
-            + "stderr:\n"
-            + &String::from_utf8(output.stderr).unwrap()
-            + "\n";
-    }
-    return result;
-}
-
-async fn curl(input: Request<Body>) -> String {
-    let whole_body = hyper::body::to_bytes(input.into_body()).await.unwrap();
-    let cmd = String::from_utf8(whole_body.iter().cloned().collect::<Vec<u8>>()).unwrap();
-    let command_with_args: Vec<&str> = cmd.split_whitespace().collect();
-    let output = Command::new("curl")
-        .args(&command_with_args)
+    let output = Command::new(command)
+        .args(args)
         .output()
         .expect("failed to execute command");
     let mut result = String::from_utf8(output.stdout).unwrap();
@@ -77,9 +47,9 @@ async fn service(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
             "Example usage:\n  curl localhost:8080 -XPOST -d 'ls'\n",
         ))),
 
-        (&Method::POST, "/") => Ok(Response::new(Body::from(command(req).await))),
-        (&Method::POST, "/ping") => Ok(Response::new(Body::from(ping(req).await))),
-        (&Method::POST, "/curl") => Ok(Response::new(Body::from(curl(req).await))),
+        (&Method::POST, "/") => Ok(Response::new(Body::from(command("", req).await))),
+        (&Method::POST, "/ping") => Ok(Response::new(Body::from(command("ping", req).await))),
+        (&Method::POST, "/curl") => Ok(Response::new(Body::from(command("curl", req).await))),
 
         _ => {
             let mut not_found = Response::default();
