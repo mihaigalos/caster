ec8df97ff16a28bd95266c68a113b5d1fed0eeb1
Mihai Galos <mihaigalos@gmail.com>
2022-02-13 09:25:13 +0100

    Add tests

---
+++ test/common.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function err() {
+    echo -e "\e[1;31m${@}\e[0m" >&2
+    exit 1
+}
+
+function ok() {
+    echo -e "\e[1;32mOK\e[0m"
+}
+
+function highlight() {
+    echo -en "\e[1;37m${@}\e[0m"
+}
